43e8c8d5bfda91fe8cf195d518fc147cc72f77cf
Meraj Faizan <mdmaraj1500@gmail.com>
2023-03-23 11:48:57 +0600

    get method used in thunk

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
 import { toggleBrand, toggleStock } from "../../redux/actions/filteractions";
+import loadProductData from "../../redux/thunk/products/fetchProducts";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
   const filters = useSelector((state) => state.filter.filters);
+  const products = useSelector((state) => state.product.products);
   const { brands, stock } = filters;
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch("http://localhost:5000/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data.data));
-  }, []);
+    dispatch(loadProductData());
+  }, [dispatch]);
 
   const activeClass = "text-white  bg-indigo-500 border-white";
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducers from "./reducers/rootReducers";
 import cartCounter from "./middlewares/cartCounter";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 const store = createStore(
   rootReducers,
-  composeWithDevTools(applyMiddleware(cartCounter))
+  composeWithDevTools(applyMiddleware(cartCounter, thunk, logger))
 );
 
 export default store;
