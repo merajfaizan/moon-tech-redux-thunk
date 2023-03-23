e30d7297374ed3d45c0af2d3861fe17b8e86ca32
Meraj Faizan <mdmaraj1500@gmail.com>
2023-03-23 10:28:58 +0600

    cart counter middleware added and used for cart ui shifting problem, explored how to add manual middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducers from "./reducers/rootReducers";
+import cartCounter from "./middlewares/cartCounter";
 
-const store = createStore(rootReducers, composeWithDevTools());
+const store = createStore(
+  rootReducers,
+  composeWithDevTools(applyMiddleware(cartCounter))
+);
 
 export default store;
